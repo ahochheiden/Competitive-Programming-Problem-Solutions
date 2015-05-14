f8b9f1f3f85fc843ac31aa511739295d9e1cf3e6
Alex Hochheiden <alexander.hochheiden@uleth.ca>
2015-05-13 21:45:08 -0600

    UVa - 10696 - f91

---
+++ 10696.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdint>
+
+using namespace std;
+
+int64_t input;
+
+int64_t f91(int64_t num);
+
+int main() {
+	while (cin >> input) {
+		if (input == 0) {
+			break;
+		}
+		cout << "f91(" << input << ") = " << f91(input) << endl;
+	}
+}
+
+int64_t f91(int64_t num) {
+	if (num <= 100) {
+		return f91(f91(num + 11));
+	} else {
+		return num - 10;
+	}
+}

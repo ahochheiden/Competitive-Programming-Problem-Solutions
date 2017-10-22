a31029a47df752a1e5dbf9a9904b386e66eaff33
Alex Hochheiden <alexander.hochheiden@uleth.ca>
2017-10-22 02:44:45 -0600

    Kattis - Quadrant Selection

---
+++ QuadrantSelection.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	int a, b;
+
+	cin >> a >> b;
+
+	if(a > 0 && b > 0)
+	{
+		cout << 1 << endl;
+	}
+
+	if(a < 0 && b > 0)
+	{
+		cout << 2 << endl;
+	}
+
+	if(a < 0 && b < 0)
+	{
+		cout << 3 << endl;
+	}
+
+	if(a > 0 && b < 0)
+	{
+		cout << 4 << endl;
+	}
+
+	return 0;
+}

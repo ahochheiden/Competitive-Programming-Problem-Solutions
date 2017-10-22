5261d46b3b26fad809b78d62da0535e3b93287b3
Alex Hochheiden <alexander.hochheiden@uleth.ca>
2017-10-22 02:49:47 -0600

    Kattis - Faktor

---
+++ Faktor.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	int a, b;
+
+	cin >> a >> b;
+
+	cout << (a * (b - 1)) + 1 << endl;
+
+	return 0;
+}

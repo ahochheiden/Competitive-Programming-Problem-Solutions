4b2abbecfbc9b0f58d27a15fc0cb66429b647dcb
Alex Hochheiden <alexander.hochheiden@uleth.ca>
2017-10-22 03:05:29 -0600

    Kattis - Stuck In A Time Loop

---
+++ StuckInATimeLoop.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int count;
+
+	cin >> count;
+
+	for(int i = 1; i <= count; i++)
+	{
+		cout << i << " Abracadabra" << endl;
+	}
+
+	return 0;
+}
